a2288e93001c17746739937e93ec7bd662492a5a
unknown <seansamu.code@gmail.com>
2024-02-29 09:43:13 -0500

    Pre-Bootstrap installation, possible dependency issues

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp, VueElement} from 'vue';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+//import '../node_modules/bootstrap/dist/css'
+
+createApp(App).mount('#app');
